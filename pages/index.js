cd4f7526e2c10aca57c5547b2a2f45a65a0d7946
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-10 11:30:09 -0300

    home/about

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <main>
+      <Header></Header>
       <h1>Malfa Criadores</h1>
+
+      <p>LOREMN LOREM LOREM LOREM</p>
 
     </main>
   )

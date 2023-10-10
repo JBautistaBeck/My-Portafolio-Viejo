23df264bc7395c6a9f74a741324bdea180f24b5c
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-10 13:46:47 -0300

    react tailwind

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 flex-reverse">
+      <h1 className="text-2xl">Mi proyecto con React y Tailwind CSS</h1>
+      <button className="bg-green-500 text-white px-4 py-2 mt-4 hover:bg-green-600">
+        Haz clic aquí
+      </button>
+    </div>
+  );
+}
+
+export default App;

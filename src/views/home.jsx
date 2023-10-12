dad1ff9cad9d3ea844d32507a01d6862c6c4e9c2
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-12 20:01:53 -0300

    Home acomodada

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -5,17 +5,17 @@ import twitter from "../utils/twitter.svg"
 
 function Home() {
   return (
-    <div className="bg-green-500 text-white p-10 h-screen">
+    <div className="bg-green-500 p-10 h-screen">
 
-      <div className="">
-        <h1 className="">Juan Bautista María Beck</h1>
-        <h2 className="">Full Stack Developer</h2>
+      <div className="flex flex-col justify-center items-center mt-20">
+        <h1 className=" text-5xl">Juan Bautista María Beck</h1>
+        <h2 className=" text-3xl mt-10">Full Stack Developer</h2>
       </div>
 
-      <div className="flex">
-        <a  href="https://www.linkedin.com/in/juan-bautista-mar%C3%ADa-beck/" target="_blank" rel="noopener noreferrer">    <img className='w-10 h-10' src={linkedin} alt='LinkedIn'/>    </a>
-        <a  href="https://github.com/JBautistaBeck" target="_blank" rel="noopener noreferrer">                              <img className='w-10 h-10' src={github} alt='GitHub'/>        </a>
-        <a  href="https://www.instagram.com/bautista.beck/" target="_blank" rel="noopener noreferrer">                      <img className='w-10 h-10' src={instagram} alt='Instagram'/>  </a>
+      <div className="flex justify-center mt-12">
+        <a  href="https://www.linkedin.com/in/juan-bautista-mar%C3%ADa-beck/" target="_blank" rel="noopener noreferrer">    <img className='w-10 h-10 ' src={linkedin} alt='LinkedIn'/>    </a>
+        <a  href="https://github.com/JBautistaBeck" target="_blank" rel="noopener noreferrer">                              <img className='w-10 h-10 ml-6' src={github} alt='GitHub'/>        </a>
+        <a  href="https://www.instagram.com/bautista.beck/" target="_blank" rel="noopener noreferrer">                      <img className='w-10 h-10 ml-6 mr-6' src={instagram} alt='Instagram'/>  </a>
         <a  href="https://twitter.com/JuanBeckFS" target="_blank" rel="noopener noreferrer">                                <img className='w-10 h-10' src={twitter} alt='Twitter'/>      </a>
       </div>
       {/* <Link className='link-boton-aboutme' to="/about"><button className='button-aboutme'>More about me</button></Link> */}

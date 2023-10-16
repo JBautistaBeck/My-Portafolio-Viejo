99e77934e54d5ad1ee0c5a1f883ab36943ce081a
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-16 15:42:59 -0300

    titulos center detail

--- src/componentes/detail.jsx
+++ src/componentes/detail.jsx
@@ -16,43 +16,31 @@ function Detail({ onClose, proyecto }) {
 
             <div className="relative bg-white p-4 w-11/12 md:w-3/4 h-5/6 text-right rounded-lg">
 
-                    <button
-                    onClick={onClose}
-                    className="absolute top-2 right-2 bg-transparent border border-black rounded-lg px-3 py-1 text-black hover:bg-black hover:text-white cursor-pointer"
-                    >
+                    <button onClick={onClose} className="absolute top-2 right-2 bg-transparent border border-black rounded-lg px-2 text-black hover:bg-black hover:text-white cursor-pointer">
                         <FontAwesomeIcon icon={faX} />
                     </button>
 
-                    <h1 className="text-2xl font-bold text-black project-name">
-                        {proyecto.nombre}
-                    </h1>
+                    <div className="flex justify-center">
+                        <h1 className="text-2xl font-bold text-black project-name"> {proyecto.nombre}</h1>
+                    </div>
 
                     <div className="flex justify-between border-b border-gray-300 mb-5">
-                        <div
-                            onClick={() => handleTabClick("fotos")}
-                            className={`flex-1 cursor-pointer ${
-                            activeTab === "fotos" ? "bg-white border-b-2 border-black" : ""
-                            } tabx`}
-                        >
+
+                        <div onClick={() => handleTabClick("fotos")} className={`flex-1 cursor-pointer ${ activeTab === "fotos" ? "bg-white border-b-2 border-black flex justify-center " : "flex justify-center"} tabx`}>
                             <h1 className="text-sm">Fotos</h1>
-                    </div>
+                        </div>
 
-                    <div
-                        onClick={() => handleTabClick("videos")}
-                        className={`flex-1 cursor-pointer ${
-                        activeTab === "videos" ? "bg-white border-b-2 border-black" : ""
-                        } tabx`}
-                    >
-                        <h1 className="text-sm">Videos</h1>
-                    </div>
+                        <div onClick={() => handleTabClick("videos")} className={`flex-1 cursor-pointer ${ activeTab === "videos" ? "bg-white border-b-2 border-black flex justify-center " : "flex justify-center"} tabx`}>
+                            <h1 className="text-sm">Videos</h1>
+                        </div>
 
-                </div>
+                    </div>
 
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
 
                     <div className={`${ activeTab === "fotos" ? "block" : "hidden" } column h-96 max-h-96 overflow-y-auto`}>
 
-                        <div className="carousel-container">
+                        <div className="">
                             <Carousel showThumbs={false} showStatus={false} itemsToShow={1} itemsToScroll={1} infiniteLoop={false} showArrows={true}>
 
                                 {proyecto.fotos.map((imagen, index) => (
@@ -72,7 +60,7 @@ function Detail({ onClose, proyecto }) {
 
 
                     <div className={`${ activeTab === "videos" ? "block" : "hidden" } column h-96 max-h-96 overflow-y-auto`}>
-                        <div className="video-container">
+                        <div className="">
                             <iframe
                                 width="100%"
                                 height="100%"
@@ -85,6 +73,7 @@ function Detail({ onClose, proyecto }) {
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
 

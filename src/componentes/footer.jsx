e155ba09c4434ea5197cb32cce9357542ea9eabd
JBautistaBeck <juanbautistabeck@gmail.com>
2023-10-17 22:31:00 -0300

    Footer responsive

--- src/componentes/footer.jsx
+++ src/componentes/footer.jsx
@@ -3,12 +3,12 @@ import { faCode, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
     return (
-      <div className="bg-gradient-to-l from-mantis-400 to-mantis-700 p-4 flex flex-col items-center justify-center">
+      <div className="bg-mantis-700 sm:bg-gradient-to-l sm:from-mantis-400 sm:to-mantis-700 p-4 flex flex-col items-center justify-center">
         
-        <h1 className=" text-5xl p-4 font-bold">Contáctame</h1>
+        <h1 className=" text-3xl sm:text-5xl p-4 font-bold">Contáctame</h1>
 
 
-        <p className=" text-lg text-center mb-7">
+        <p className=" sm:text-lg text-center mb-7">
             Soy un programador en constante aprendizaje. Si estás interesado en colaborar o tienes alguna pregunta, no dudes en contactarme. Puedes escribirme a mi dirección de correo electrónico: 
               <a href="mailto:juanbautistabeck@gmail.com" className="text-blue-900 hover:underline"> juanbautistabeck@gmail.com</a>.
               También puedes conectar conmigo en mis perfiles de redes sociales.
